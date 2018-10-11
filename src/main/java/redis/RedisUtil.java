5f807c87a174df939f98238869dc64400aeaddc6
zakiboy <13636396841@163.com>
2018-10-11 17:04:57 +0800

    redis工具对象

---
+++ src/main/java/redis/RedisUtil.java
@@ -0,0 +1,71 @@
+package com.org.zyy.redis;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+
+public class RedisUtil {
+
+	//Redis服务器IP
+	private static String ADDR = "127.0.0.1";
+
+	//Redis的端口号
+	private static int PORT = 6379;
+
+	//访问密码
+	private static String AUTH = "admin";
+
+	//控制一个pool最多有多少个状态为idle(空闲的)的jedis实例，默认值也是8。
+	private static int MAX_IDLE = 200;
+
+	private static int TIMEOUT = 10000;
+
+	//在borrow一个jedis实例时，是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的；
+	private static boolean TEST_ON_BORROW = true;
+
+	private static JedisPool jedisPool = null;
+
+	/**
+	 * 初始化Redis连接池
+	 */
+	static {
+		try {
+			JedisPoolConfig config = new JedisPoolConfig();
+			config.setMaxIdle(MAX_IDLE);
+			config.setTestOnBorrow(TEST_ON_BORROW);
+			jedisPool = new JedisPool(config, ADDR, PORT, TIMEOUT, AUTH);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 获取Jedis实例
+	 * @return
+	 */
+	public synchronized static Jedis getJedis() {
+		try {
+			if (jedisPool != null) {
+				Jedis resource = jedisPool.getResource();
+				return resource;
+			} else {
+				return null;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	/**
+	 * 释放jedis资源
+	 * @param jedis
+	 */
+	public static void returnResource(final Jedis jedis) {
+		if (jedis != null) {
+			jedis.close();
+			jedisPool.close();
+		}
+	}
+}
